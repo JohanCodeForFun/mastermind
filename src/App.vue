1226c48b70c1be24ebedb3ff06c5359fe2748f30
JohanCodeForFun <hellberg.johan@hotmail.com>
2024-11-17 22:52:28 +0100

    generate content

--- src/App.vue
+++ src/App.vue
@@ -5,9 +5,30 @@ import TheCounter from './components/TheCounter.vue'
 import BlogArticle from './components/BlogArticle.vue'
 
 const posts = ref([
-  { id: 1, title: 'Blog #1', content: 'Lorem ipsum' },
-  { id: 2, title: 'Blog #1', content: 'Lorem ipsum' },
-  { id: 3, title: 'Blog #1', content: 'Lorem ipsum' },
+  { id: 1, title: 'Blog #1', content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.' },
+  {
+    id: 2,
+    title: 'Blog #2',
+    content: 'Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+  },
+  {
+    id: 3,
+    title: 'Blog #3',
+    content:
+      'Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
+  },
+  {
+    id: 4,
+    title: 'Blog #4',
+    content:
+      'Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.',
+  },
+  {
+    id: 5,
+    title: 'Blog #5',
+    content:
+      'Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+  },
 ])
 </script>
 
