97aae00c6d652e8fd20d3ebdcbc55c902432e629
JohanCodeForFun <hellberg.johan@hotmail.com>
2024-11-17 22:36:23 +0100

    add counter component

---
+++ src/components/TheCounter.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+defineProps(['title'])
+
+const count = ref(0)
+
+// const incrementCount = () => {
+//   count.value = count.value++
+// }
+</script>
+
+<template>
+  <h2>{{ title }}</h2>
+  <button @click="count++">You clicked me {{ count }} times</button>
+</template>
