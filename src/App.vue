6c809d408c2dc0b85925567bb9ac01a1920a06c7
JohanCodeForFun <hellberg.johan@hotmail.com>
2024-11-17 22:36:15 +0100

    remove boilerplate and add blog component

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,29 @@
 <script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
+import { ref } from 'vue'
+
+import TheCounter from './components/TheCounter.vue'
+import BlogArticle from './components/BlogArticle.vue'
+
+const posts = ref([
+  { id: 1, title: 'Blog #1', content: 'Lorem ipsum' },
+  { id: 2, title: 'Blog #1', content: 'Lorem ipsum' },
+  { id: 3, title: 'Blog #1', content: 'Lorem ipsum' },
+])
 </script>
 
 <template>
   <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
+    <!-- <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" /> -->
 
     <div class="wrapper">
-      <HelloWorld msg="You did it!" />
+      <!-- <HelloWorld msg="You did it!" /> -->
     </div>
   </header>
 
   <main>
-    <TheWelcome />
+    <TheCounter title="Counter Component" />
+    <BlogArticle v-for="post in posts" :key="post.id" :title="post.title" :content="post.content" />
+    <!-- <TheWelcome /> -->
   </main>
 </template>
 
---
+++ src/components/BlogArticle.vue
@@ -0,0 +1,7 @@
+<script setup lang="ts">
+defineProps(['title', 'content'])
+</script>
+<template>
+  <h3>{{ title }}</h3>
+  <p>{{ content }}</p>
+</template>
